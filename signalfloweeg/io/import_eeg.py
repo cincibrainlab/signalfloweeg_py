3bee990c54903d2afb6c8ef92782625d8b7ff27a
drpedapati <ernest.pedapati@cchmc.org>
2024-03-12 17:10:43 +0000

    Update book structure and add new chapters

--- signalfloweeg/io/import_eeg.py
+++ signalfloweeg/io/import_eeg.py
@@ -34,6 +34,7 @@ def load_eeg(file_path=None, recording_type=None):
 
     # Display information on how to specify the recording type for the function with colors for better readability
     if file_path is None or recording_type is None:
+        print(f"Check Recording Type ...")
         display_info(import_specs)
         return None
 
@@ -45,11 +46,25 @@ def load_eeg(file_path=None, recording_type=None):
         display_info(import_specs)
         return None
     print(recording_type)
+    
+    # Add additional conditions for recording types here
     # Call the appropriate import function
-    if recording_type == "EGI_128_RAW":
+    if recording_type == "EEGLAB_RAW_SET":
+        EEG = import_eeglab(file_path, recording_type)
+    elif recording_type == "EEGLAB_EPOCHS_SET":
+        EEG = import_eeglab(file_path, recording_type)
+    
+    elif recording_type == "MNE_FIF":
+        EEG = import_mne(file_path, recording_type)            
+    
+    elif recording_type == "EGI_128_RAW":
         EEG = import_egi(file_path, recording_type)
-    elif recording_type == "import_mea30":
-        EEG = import_mea30(file_path)
+    elif recording_type == "EGI_128_MFF":
+        EEG = import_egi(file_path, recording_type)
+    
+    elif recording_type == "NEURONEXUS_30_EDF":
+        EEG = import_neuronexus(file_path, recording_type)
+        
     # Add more import functions for other recording types
     else:
         print(f"No import function found for recording type '{recording_type}'.")
@@ -79,6 +94,35 @@ def display_info(import_specs):
         "\033[1;35mPlease specify the recording type by passing it as the 'recording_type' argument.\n\033[0m"
     )
 
+def import_eeglab(file_path, recording_type):
+    """
+    Import EEGLAB recording, apply montage, and perform epoching if necessary.
+    """
+    if recording_type == "EEGLAB_RAW_SET":
+        print("Importing EEGLAB RAW SET data...")
+        try:
+            EEG = mne.io.read_raw_eeglab(file_path, preload=True)
+        except TypeError:
+            print("Error: The specified file contains epochs. Please use 'EEGLAB_EPOCHS_SET' as the recording type.")
+            return None
+
+    if recording_type == "EEGLAB_EPOCHS_SET":
+        print("Importing EEGLAB EPOCHS SET data...")
+        EEG = mne.io.read_epochs_eeglab(file_path)
+
+    return EEG
+
+def import_mne(file_path, recording_type):
+    """
+    Import MNE FIF recording, apply montage, and perform epoching if necessary.
+    """
+    if recording_type == "MNE_FIF":
+        print("Importing MNE FIF data...")
+        EEG = mne.io.read_raw_fif(file_path, preload=True)
+        montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
+        EEG.set_montage(montage, match_case=False)
+
+    return EEG
 
 def import_egi(file_path, recording_type):
     """
@@ -86,44 +130,59 @@ def import_egi(file_path, recording_type):
     """
 
     if recording_type == "EGI_128_RAW":
-        print("EGI_128_RAW")
+        print("Importing EGI 128 Channel RAW data...")
         # Implement the specific steps for importing EGI128 data
-        raw = mne.io.read_raw_egi(file_path, preload=True)
-        montage = mne.channels.make_standard_montage("GSN-HydroCel-128")
-        raw.set_montage(montage)
+        raw = mne.io.read_raw_egi(input_fname=file_path, preload=False)
+        montage = mne.channels.make_standard_montage("GSN-HydroCel-129")
+        montage.ch_names[128] = "E129"
+        raw.set_montage(montage, match_case=False)
 
-    # Perform any additional preprocessing or epoching steps
+    if recording_type == "EGI_128_SET":
+        print("Importing EGI 128 Channel EEGLAB SET data...")
+        try:
+            epochs = mne.io.read_epochs_eeglab(file_path)
+        except Exception as e:
+            print(f"Failed to read raw EEG data, will attempt import with Epochs: {e}")
+            raw = mne.io.read_raw_eeglab(file_path, preload=True)
+
+    # This code block is not used in the script. It shows how to load
+    # continuous EEG data from a .set file with preload=True. Preloading
+    # is useful for preprocessing that needs the full dataset before epoching.
+
     return raw
 
 
-def import_mea30(file_path):
+def import_neuronexus(file_path, recording_type):
     """
-    Import MEA30 recording, apply montage, and perform epoching if necessary.
+    Import neuronexus recording, apply montage, and perform epoching if necessary.
     """
-    # Implement the specific steps for importing MEA30 data
-    raw = mne.io.read_raw_edf(file_path, preload=True)
-    montage = mne.channels.make_standard_montage("standard_1020")
-    raw.set_montage(montage)
-    # Perform any additional preprocessing or epoching steps
-    return raw
+    EEG = []
+    if recording_type == "NEURONEXUS_30_EDF":
+        # Add code to import the Neuronexus 30-channel Multielectrode Array
+        # Return an MNE object with the correct channel montage and event markers
+        EEG = mne.io.read_raw_edf(file_path, preload=True)
 
+    elif recording_type == "NEURONEXUS_30_XDF":
+        # Add code to import the Neuronexus 30-channel Multielectrode Array
+        # Return an MNE object with the correct channel montage and event markers
+        pass
+   
+    return EEG
 
-## Add more import functions for other recording types
-# load_eeg(continuous_eeg_file, "EGI128A")
+if __name__ == "__main__":
 
+    from signalfloweeg.utils import load_catalog
 
-from signalfloweeg.utils import load_catalog
+    # Using utils.load_catalog() to load an example EEG file
+    catalog = load_catalog(
+        "https://raw.githubusercontent.com/cincibrainlab/signalfloweeg_py/master/userdata/cchmc_data.yaml"
+    )
+    from signalfloweeg.io import load_eeg
 
-# Using utils.load_catalog() to load an example EEG file
-catalog = load_catalog(
-    "https://raw.githubusercontent.com/cincibrainlab/signalfloweeg_py/master/userdata/cchmc_data.yaml"
-)
+    # EEGLAB SET FILE (CONTINUOUS)
+    eeglab_set = catalog["demo_infant_resting"]
+    eeglab_mne = load_eeg(file_path=eeglab_set,  recording_type = "EEGLAB_RAW_SET")
 
-# EGI/MAGSTIM 128 Resting State Data
-resting_raw = catalog["demo_rest_raw"]
-
-# EGI/MAGSTIM 128 Auditory Evoked Potentials
-erp_raw = catalog["demo_auditory_chirp"]
-
-# No arguments to show recording types
-load_eeg(resting_raw, "EGI_128_RAW")
+    # EEGLAB SET FILE (EPOCHED)
+    eeglab_set_epoched = catalog["demo_auditory_chirp"]
+    eeglab_mne_epoched = load_eeg(eeglab_set_epoched, "EEGLAB_EPOCHS_SET")
