68c49951976ca94b1c2f6dcaeeaa0fcf413282d0
Nathan Suer <natesuer@gmail.com>
2024-05-20 12:16:11 -0400

    Cleaning up
    Cleaning up un needed files

--- .bash_history
+++
@@ -1,9 +0,0 @@
-sudo
-apt update
-sudo apt update
-su
-sudo passwd root
-su
-sudo -i
-su -i
-sudo passwd
--- Inbox/ChirpSpectralEventsPython/j110_resting_load_epochs.jl
+++
@@ -1,5 +0,0 @@
-using NeuroAnalyzer
-
-resting_file = "D0348_rest_postica.set"
-cd("/Users/ernie/Documents/ExampleData/APD")
-eeg = import_recording(resting_file)
