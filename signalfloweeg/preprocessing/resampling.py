22ab4acaffa018cffcce8292b6a77fbb776253a9
ggammoh <gammohgb@mail.uc.edu>
2024-05-21 09:06:17 -0400

    Update resampling.py

--- signalfloweeg/preprocessing/resampling.py
+++ signalfloweeg/preprocessing/resampling.py
@@ -9,7 +9,18 @@ import numpy as np
 import mne
 
 # Functions and classes
-def resample(data:mne.io.Raw, sampling_freq:float = 500):
+def resample(data: mne.io.Raw, sampling_freq: float = 500):
+    """
+    Resamples the given data to the specified sampling frequency.
+
+    Parameters:
+        data (mne.io.Raw): The input data to be resampled.
+        sampling_freq (float): The desired sampling frequency (in Hz) for the resampled data. Default is 500 Hz.
+
+    Returns:
+        mne.io.Raw: The resampled data.
+
+    """
     raw_downsampled_poly = data.copy().resample(
         sfreq=sampling_freq,
         method="polyphase",
